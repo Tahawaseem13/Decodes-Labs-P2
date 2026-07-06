9b7713c54f92a7a863a763a9490ad65f31413679
Taha Waseem <tahawaseem12118@gmail.com>
2026-07-06 16:34:31 +0500

    prepare dataset for Training

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 # Load Iris Dataset
 iris = load_iris()
@@ -33,3 +34,28 @@ print(df.describe())
 
 print("\n========== Species Count ==========")
 print(df["species"].value_counts())
+
+X = iris.data
+
+y = iris.target
+
+print("\n========== Features (First 5 Rows) ==========")
+print(X[:5])
+
+print("\n========== Labels (First 5 Values) ==========")
+print(y[:5])
+# Split Dataset
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    shuffle=True
+)
+
+print("\n========== Training Data ==========")
+print(X_train.shape)
+
+print("\n========== Testing Data ==========")
+print(X_test.shape)
